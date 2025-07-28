0ce7d596d1dbf663f23b3d9d1b6f311265c4d988
EIFY <chuanchih@gmail.com>
2025-07-27 17:27:23 -0700

    rename, refactor, simplify, experiment, and improve the plots
    It's better to keep the singular vector across the steps even with repeated spectral hammer

--- spectral_chisel.py
+++ spectral_chisel.py
@@ -4,44 +4,43 @@ import math
 import torch
 
 
+def power_iter(mat, v, steps):
+    for _ in range(steps):
+        u = mat @ v
+        v = mat.mT @ u
+        v /= torch.linalg.vector_norm(v)
+    return v
+
+
 class SpectralChisel:
 
-    def _iterate(self, mat, steps):
-        for _ in range(steps):
-            u = mat @ self.v
-            self.v = mat.mT @ u
-            self.v /= torch.linalg.vector_norm(self.v)
-
     def __init__(self, mat, steps=5):
-        self.v = torch.normal(0, 1, (mat.size(1),)).to(mat)
-        self._iterate(mat, steps)
+        n = mat.size(1)
+        v = torch.normal(0, n**-0.5, (n,)).to(mat)
+        self.v = power_iter(mat, v, steps)
 
-    def shave(self, mat, s_max, steps=math.inf, iter_per_step=1):
+    def hammer(self, mat, s_max, steps=math.inf, iter_per_step=1):
         it = range(steps) if steps < math.inf else itertools.repeat(None)
         for _ in it:
-            self._iterate(mat, iter_per_step - 1)
+            self.v = power_iter(mat, self.v, iter_per_step - 1)
             u = mat @ self.v
             self.v = mat.mT @ u
             v_norm, u_norm = torch.linalg.vector_norm(self.v), torch.linalg.vector_norm(u)
             self.v /= v_norm
+            u /= u_norm
             spectral_norm = v_norm / u_norm
-            if spectral_norm > s_max:
-                u /= u_norm
-                mat -= (spectral_norm - s_max) * torch.outer(u, self.v)
-            else:
-                return
+            mat -= (spectral_norm - s_max) * torch.outer(u, self.v)
+        return mat
 
-    def weight_decay(self, mat, wd, s_max=0., steps=math.inf, iter_per_step=1):
+    def decay(self, mat, wd, steps=math.inf, iter_per_step=1):
         it = range(steps) if steps < math.inf else itertools.repeat(None)
         for _ in it:
-            self._iterate(mat, iter_per_step - 1)
+            self.v = power_iter(mat, self.v, iter_per_step - 1)
             u = mat @ self.v
             self.v = mat.mT @ u
             v_norm, u_norm = torch.linalg.vector_norm(self.v), torch.linalg.vector_norm(u)
             self.v /= v_norm
+            u /= u_norm
             spectral_norm = v_norm / u_norm
-            if spectral_norm > s_max:
-                u /= u_norm
-                mat -= spectral_norm * wd * torch.outer(u, self.v)
-            else:
-                return
+            mat -= spectral_norm * wd * torch.outer(u, self.v)
+        return mat
